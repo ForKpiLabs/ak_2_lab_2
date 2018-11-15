f30e65c9166bef71f232e6d5e30f10a3483ad315
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,12 @@
 #include "calculator.h"
-
+// something here
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
-
+// Verbovska Eva IO-03
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+// lab for ak-2
